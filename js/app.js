1e3c78f5e1b15373fcd1944ae38297551d578afc
serdjukow <serduykovs@yandex.ru>
2024-06-23 20:38:16 +0200

    image-popup

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
-window.onload = function() {
+window.onload = function () {
+  setTimeout(function () {
+    document.body.classList.add("loaded")
 
-	setTimeout(function() {
+    if (window.matchMedia("(min-width: 992px)").matches) {
+      // If not mobile
 
-		document.body.classList.add('loaded')
-
-		Draggable.create('.gallery', {
-			bounds: 'body',
-			inertia: true
-		})
-
-	}, 200)
-
+      Draggable.create(".gallery", {
+        bounds: "body",
+        inertia: true,
+      })
+    }
+  }, 200)
 }
+
+document.querySelectorAll(".gallery__item").forEach(function (e) {
+  let img = new Image(),
+    hrefURL = e.getAttribute("href")
+  img.onload = function () {
+    e.dataset.pswpWidth = this.width
+    e.dataset.pswpHeight = this.height
+  }
+  img.src = hrefURL
+})
+
+import PhotoSwipeLightbox from "../libs/PhotoSwipe/photoswipe-lightbox.esm.min.js"
+const lightbox = new PhotoSwipeLightbox({
+  gallery: ".gallery",
+  children: ".gallery__item",
+  pswpModule: () => import("../libs/PhotoSwipe/photoswipe.esm.min.js"),
+})
+lightbox.init()
